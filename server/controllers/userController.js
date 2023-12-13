b1e0a4141d6b730d01f2da5b74d9b0be41f89905
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-13 22:58:58 +0545

    logged out and cookie destroyed.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -47,6 +47,10 @@ export const registerUser = asyncHandler(async (req, res) => {
 });
 
 export const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
   res.status(200).json({ message: "logout user" });
 });
 
