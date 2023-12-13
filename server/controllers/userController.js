65c61b21947756b08cc753abbeafdfc6b9c5c8b4
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-13 22:42:41 +0545

    password is hashed and token is generated and stored in cookies.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,13 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import { generateToken } from "../utils/generateTokens.js";
 
-export const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+export const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
-  if (existingUser) {
-    res.status(400).json({ message: "User already exists" });
-  }
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
-  if (user) {
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPasswords(password))) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -26,8 +20,30 @@ export const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth user" });
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400).json({ message: "User already exists" });
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400).json({
+      message: "Invalid User data",
+    });
+  }
 });
 
 export const logoutUser = asyncHandler(async (req, res) => {
---
+++ server/utils/generateTokens.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "10d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+};
