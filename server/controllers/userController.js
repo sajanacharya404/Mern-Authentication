ba7d046f56b7c5134777b9e57f11be4bbf67e2af
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-14 16:03:30 +0545

    Auth:Authentication added to the user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -55,8 +55,30 @@ export const logoutUser = asyncHandler(async (req, res) => {
 });
 
 export const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get user" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 export const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
   res.status(200).json({ message: "Update user" });
 });
