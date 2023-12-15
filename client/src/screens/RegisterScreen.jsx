e38a895f4e689f7712db21b59889a4d4c06ba3fd
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-15 18:29:50 +0545

    Update : register page done using reduxtoolkit

--- client/src/screens/RegisterScreen.jsx
+++ client/src/screens/RegisterScreen.jsx
@@ -1,5 +1,10 @@
-import { React, useState } from "react";
-import { Link } from "react-router-dom";
+import { React, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
+import { useRegisterMutation } from "../slices/userApiSlice";
 
 const RegisterScreen = () => {
   const [name, setName] = useState("");
@@ -7,8 +12,33 @@ const RegisterScreen = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const submitHandler = (e) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Password do not match");
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate("/login");
+        toast("Register successfully");
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
@@ -21,13 +51,13 @@ const RegisterScreen = () => {
           width: "100%",
         }}
       >
-        <h1>Register</h1>
+        <h2 style={{ textAlign: "center", marginTop: "30px" }}>Register</h2>
         <form
           action=""
           onSubmit={submitHandler}
           style={{
             display: "flex",
-            justifyContent: "space-evenly",
+            justifyContent: "space-between",
             alignItems: "center",
             flexDirection: "column",
 
@@ -35,36 +65,54 @@ const RegisterScreen = () => {
           }}
         >
           <input
-            type="email"
+            type="text"
             placeholder="Enter Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            style={{ padding: "10px", borderRadius: "20px" }}
+            style={{
+              padding: "10px",
+              borderRadius: "20px",
+              marginBottom: "10px",
+            }}
           />
           <input
             type="email"
             placeholder="Enter Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            style={{ padding: "10px", borderRadius: "20px" }}
+            style={{
+              padding: "10px",
+              borderRadius: "20px",
+              marginBottom: "10px",
+            }}
           />
           <input
             type="Pasword"
             placeholder="Enter Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            style={{ padding: "10px", borderRadius: "20px" }}
+            style={{
+              padding: "10px",
+              borderRadius: "20px",
+              marginBottom: "10px",
+            }}
           />
           <input
-            type="email"
+            type="password"
             placeholder="Enter Password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             style={{ padding: "10px", borderRadius: "20px" }}
           />
+          {isLoading && <Loader />}
           <button
             type="submit"
-            style={{ padding: "10px", borderRadius: "20px", border: "none" }}
+            style={{
+              padding: "10px",
+              borderRadius: "20px",
+              border: "none",
+              marginTop: "20px",
+            }}
           >
             Submit
           </button>
--- client/src/slices/userApiSlice.js
+++ client/src/slices/userApiSlice.js
@@ -16,7 +16,15 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: "POST",
       }),
     }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
+  usersApiSlice;
