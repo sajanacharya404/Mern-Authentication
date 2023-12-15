e459d6dd69e722467665e7c1082e7ecd35b3f8b7
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-15 19:08:25 +0545

    Backend finished

--- server/utils/generateTokens.js
+++ server/utils/generateTokens.js
@@ -8,7 +8,7 @@ export const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "None",
+    sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
